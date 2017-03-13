74a44f26ce130865d73bc78c69a930bcbf9f7136
8422 <gmohamedabdulla@gmail.com>
2017-03-13 15:33:04 +0530

    vectorised implementation  Please enter the commit message for your changes. Lines starting

--- linear regresion/src/linear_regression.py
+++ linear regresion/src/linear_regression.py
@@ -1,26 +1,27 @@
 '''
-	Linear regression using loops
+	Naive Linear regression using loops
 '''
 import numpy as np
 import matplotlib.pyplot as plt  
 import time
 
 def read_data():
+	'''
+		Reading the data
+	'''
 	x_train = np.loadtxt('../data/x_train.csv')
 	y_train = np.loadtxt('../data/y_train.csv')
-	x_test = np.loadtxt('../data/x_test.csv')
-	y_test = np.loadtxt('../data/y_test.csv')
 
-	return x_train, y_train, x_test, y_test
+	#preprocessing the data
+	x_train = np.log(x_train)
 
-def get_error(x_train, y_train, w1, w2):
-	error = 0
-	for x,y in zip(x_train, y_train):
-		y_pred = w1 + x * w2
-		error += (y - y_pred) ** 2
-	return error / len(x_train)
+
+	return x_train, y_train
 
 def get_gradient(x_train, y_train, w1, w2):
+	'''
+		compute the gradient of the loss function
+	'''
 	dw1 = 0
 	dw2 = 0
 	n = float(len(x_train))
@@ -39,15 +40,12 @@ def create_model(x_train, y_train, epochs = 10000, learning_rate = 0.001):
 			y = w1 + x * w2
 
 		where w1 and w2 are the parameters of the model which we want to learn
-	'''
-
-	error_list =[]
-	
+	'''	
 	w1 = 0
 	w2 = 0
 
-
 	print('model is running... ')
+	
 	for i in range(epochs):
 		#get the gradients 
 		dw1 , dw2 = get_gradient(x_train, y_train, w1, w2)
@@ -56,35 +54,30 @@ def create_model(x_train, y_train, epochs = 10000, learning_rate = 0.001):
 		w1 = w1 - (learning_rate * dw1)
 		w2 = w2 - (learning_rate * dw2)
 
-		#get error 
-		#error = get_error(x_train, y_train, w1, w2)
-		#error_list.append(error)
-
 	return w1, w2
 
-if __name__ == '__main__':
+def main():
+	x_train, y_train = read_data()
 
-	x_train, y_train, x_test, y_test = read_data()
-
-	x_train = np.log(x_train)
-	x_test = np.log(x_test)
-
-
+	#create the model
 	start_time = time.time()
 	w1,w2 = create_model(x_train, y_train)
 	print(time.time() - start_time)
 
+	#calculate the predicted values using our model
 	y_pred = []
 	for x in x_train:
 		y = w1 + x * w2
 		y_pred.append(y)
 
-	print(w1,w2)
-
-	#plot the graph
+	#plot the graph showing actual values and our prediction
 	plt.scatter(x_train, y_train, color='red')
 	plt.plot(x_train, y_pred, color='blue')
 	plt.show()
+
+
+if __name__ == '__main__':
+	main()
 
 
 
--- linear regresion/src/linear_regression_vec.py
+++ linear regresion/src/linear_regression_vec.py
@@ -1,81 +1,81 @@
 '''
-	Vectorised Linear regression 
+    Vectorised Linear regression 
 '''
 import numpy as np
 import matplotlib.pyplot as plt  
 import time
 
 def read_data():
-	x_train = np.loadtxt('../data/x_train.csv')
-	y_train = np.loadtxt('../data/y_train.csv')
+    x_train = np.loadtxt('../data/x_train.csv')
+    y_train = np.loadtxt('../data/y_train.csv')
 
-	#preprocessing the data
-	x_train = np.log(x_train)
+    #preprocessing the data
+    x_train = np.log(x_train)
 
-	return x_train, y_train, x_test, y_test
+    return x_train, y_train
 
-def get_gradient(X, Y, W):
-	'''
-		compute the gradient of the loss function
-	'''
-	dW = np.array([0,0])
-	dW = dW.reshape(2,1)
-	n = float(len(x_train))
-	
-	dW = (2.0/n) * (X.T.dot(X).dot(W) - X.T.dot(Y))
+def get_gradient(X, Y, W, n):
+    '''
+        compute the gradient of the loss function
+    '''
+    dW = np.array([0,0])
+    dW = dW.reshape(2,1)
+    
+    
+    dW = (2.0/n) * (X.T.dot(X).dot(W) - X.T.dot(Y))
 
-	return dW
-
-
-def create_model(X_train, Y_train, epochs = 10000, learning_rate = 0.001):
-	'''
-		fit a linear model such that
-
-			y = w1 + x * w2
-
-		where w1 and w2 are the parameters of the model which we want to learn
-	'''
-	
-	W = np.array([0,0])
-	W = W.reshape(2,1)
+    return dW
 
 
-	print('model is running... ')
-	for i in range(epochs):
-		#get the gradients 
-		dW = get_gradient(X_train, Y_train, W)
+def create_model(X_train, Y_train, n,epochs = 10000, learning_rate = 0.001):
+    '''
+        fit a linear model such that
 
-		#update the weights
-		W = W - learning_rate * dW
-	
-	return W[0][0], W[1][0], W
+            y = w1 + x * w2
+
+        where w1 and w2 are the parameters of the model which we want to learn
+    '''
+    
+    W = np.array([0,0])
+    W = W.reshape(2,1)
+
+
+    print('model is running... ')
+    for i in range(epochs):
+        #get the gradients 
+        dW = get_gradient(X_train, Y_train, W, n)
+
+        #update the weights
+        W = W - learning_rate * dW
+    
+    return W[0][0], W[1][0], W
 
 
 def main():
-	x_train, y_train = read_data()
-	Y_train = y_train.reshape(y_train.shape[0],1)
+    x_train, y_train = read_data()
+    Y_train = y_train.reshape(y_train.shape[0],1)
 
-	#Converting the array to a matrix and adding a column with 1
-	temp = x_train.reshape(x_train.shape[0],len(x_train[0]))
-	X_train = np.ones((temp.shape[0],temp.shape[1]+1))
-	X_train[:,1:] = temp
-	
-	#Run the model
-	start_time = time.time()
-	w1,w2, W = create_model(X_train, Y_train)
-	print(time.time() - start_time)
+    #Converting the array to a matrix and adding a column with 1
+    temp = x_train.reshape(x_train.shape[0],1)
+    X_train = np.ones((temp.shape[0],temp.shape[1]+1))
+    X_train[:,1:] = temp
+    
+    #Run the model
+    start_time = time.time()
+    n = float(len(x_train))
+    w1,w2, W = create_model(X_train, Y_train,n)
+    print(time.time() - start_time)
 
-	#compute the model prediction
-	y_pred = X_train.dot(W)
+    #compute the model prediction
+    y_pred = X_train.dot(W)
 
-	#plot the graph
-	plt.scatter(x_train, y_train, color='red')
-	plt.plot(x_train, y_pred, color='blue')
-	plt.show()
+    #plot the graph
+    plt.scatter(x_train, y_train, color='red')
+    plt.plot(x_train, y_pred, color='blue')
+    plt.show()
 
 if __name__ == '__main__':
-
-	
+    main()
 
 
 
